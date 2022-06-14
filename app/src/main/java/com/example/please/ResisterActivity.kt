3152c7829885ca640f9a7ad229191a3c9c5bc3ae
jjamk <59911373+jjamk@users.noreply.github.com>
2022-06-15 02:01:57 +0900

    resister_start

---
+++ app/src/main/java/com/example/please/ResisterActivity.kt
@@ -0,0 +1,11 @@
+package com.example.please
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ResisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_resister)
+    }
+}
